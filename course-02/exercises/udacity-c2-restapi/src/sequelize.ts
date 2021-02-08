58a95186541624f194eb603e0ec144f945f69d23
sandeep kesarwani <sandeep.kesarwani@mechademy.com>
2021-02-08 16:42:23 +0530

    UPDATE: env setup

--- course-02/exercises/udacity-c2-restapi/src/sequelize.ts
+++ course-02/exercises/udacity-c2-restapi/src/sequelize.ts
@@ -1,6 +1,8 @@
 import {Sequelize} from 'sequelize-typescript';
 import { config } from './config/config';
 
+require('dotenv').config();
+
 // Instantiate new Sequelize instance!
 export const sequelize = new Sequelize({
   "username": process.env.USERNAME,
